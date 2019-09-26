3b5f05fd8989d92b14591b60e819f7eaf0872c0d
AkshayRanadeAM6007 <55832035+AkshayRanadeAM6007@users.noreply.github.com>
2019-09-26 10:33:11 +0100

    Added solution for the Infinite Monkey Theorem

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace InfiniteMonkeyTheorem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomN, strLength = 100;
+
+            StringBuilder monkeyBusiness = new StringBuilder("");
+
+            
+
+            for (int i=0; i<strLength; i++)
+            {
+                randomN = random.Next(97, 123);
+                monkeyBusiness.Append((char)randomN);
+            }
+
+            Console.WriteLine(monkeyBusiness);
+            
+
+            
+        }
+    }
+}
